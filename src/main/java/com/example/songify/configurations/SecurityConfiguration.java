730521ac4e78faf153ccac31bfa6926cdafa14dc
vigoliet <vigor.lundkvist@live.se>
2024-09-17 15:15:18 +0200

    Implemented security for songify
    Added login feature for the application.

---
+++ src/main/java/com/example/songify/configurations/SecurityConfiguration.java
@@ -0,0 +1,25 @@
+package com.example.songify.configurations;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+public class SecurityConfiguration {
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http.csrf(csrf -> csrf
+                        .ignoringRequestMatchers("/api/addSong"))
+                .authorizeHttpRequests(authz -> authz
+                        .requestMatchers("/", "/styles.css", "/api/songs").permitAll()
+                        .requestMatchers(HttpMethod.POST, "/api/addSong").permitAll()
+                        .anyRequest().authenticated())
+                .oauth2Login(Customizer.withDefaults())
+                .logout(httpSecurityLogoutConfigurer -> httpSecurityLogoutConfigurer.logoutSuccessUrl("/"));
+        return http.build();
+    }
+}
