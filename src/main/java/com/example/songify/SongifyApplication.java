4b03dc7b3e22dae00b9d35f228e72e7bccc4c14a
Vigoliet <vigor.lundkvist@live.se>
2024-09-20 14:31:05 +0200

    Updated imports

--- src/main/java/com/example/songify/SongifyApplication.java
+++ src/main/java/com/example/songify/SongifyApplication.java
@@ -1,12 +1,7 @@
-// src/main/java/com/example/songify/SongifyApplication.java
 package com.example.songify;
 
-import io.github.cdimascio.dotenv.Dotenv;
-import io.github.cdimascio.dotenv.DotenvEntry;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
-import java.util.Map;
 
 @SpringBootApplication
 public class SongifyApplication {
