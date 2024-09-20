0f6a23e9409d1277523714d9e62bca0443b50311
Vigoliet <vigor.lundkvist@live.se>
2024-09-20 14:10:53 +0200

    Updated project structure
    New attempt using ENV locally & on CodeBuild.
    
    Also added and created EC2, RDS and changed Cognito URLS.

--- src/main/java/com/example/songify/SongifyApplication.java
+++ src/main/java/com/example/songify/SongifyApplication.java
@@ -1,13 +1,24 @@
+// src/main/java/com/example/songify/SongifyApplication.java
 package com.example.songify;
 
+import io.github.cdimascio.dotenv.Dotenv;
+import io.github.cdimascio.dotenv.DotenvEntry;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import java.util.Map;
 
 @SpringBootApplication
 public class SongifyApplication {
 
+	static {
+		Dotenv dotenv = Dotenv.load();
+		for (DotenvEntry entry : dotenv.entries()) {
+			System.setProperty(entry.getKey(), entry.getValue());
+		}
+	}
+
 	public static void main(String[] args) {
 		SpringApplication.run(SongifyApplication.class, args);
 	}
-
 }
